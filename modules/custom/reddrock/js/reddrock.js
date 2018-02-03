1ce20e7c7e593f386d096f6cc924bc147de6afa7
cperry-adv <cperry@advanstar.com>
2018-02-02 17:55:20 -0800

    ya

---
+++ modules/custom/reddrock/js/reddrock.js
@@ -0,0 +1,15 @@
+(function ($, window, Drupal, drupalSettings) {
+
+  'use strict';
+
+  /**
+   * Redirect to the selected show's blog listing page.
+   *
+   * @type {{attach: Function}}
+   */
+  Drupal.behaviors.reddrock = {
+    attach: function(context, settings) {
+      
+    }
+  }
+})(jQuery, this, Drupal, drupalSettings);
