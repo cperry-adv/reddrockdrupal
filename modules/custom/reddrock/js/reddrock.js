20ad488ed6ddc0204389f13788c8804d253d1170
cperry-adv <cperry@advanstar.com>
2018-02-02 18:32:47 -0800

    ya

--- modules/custom/reddrock/js/reddrock.js
+++ modules/custom/reddrock/js/reddrock.js
@@ -9,7 +9,14 @@
    */
   Drupal.behaviors.reddrock = {
     attach: function(context, settings) {
-      alert('ho');
+      var mySound = document.createElement('audio');
+    mySound.setAttribute('src', 'http://dev.reddrockmusic.com/sites/default/files/2018-02/drumtakes%201-Audio.wav');
+
+    //play the sound when the visitor moves the mouse over
+    //an element with the class 'picture'. 
+    $('.menu--account > li').mouseover(function() {
+        mySound.play();
+    });
     }
   }
 })(jQuery, this, Drupal, drupalSettings);
