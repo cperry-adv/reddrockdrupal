8a04fd56c75e974059381d08e19b0ae18fdf8bdc
cperry-adv <cperry@advanstar.com>
2018-02-02 18:35:09 -0800

    ya

--- modules/custom/reddrock/js/reddrock.js
+++ modules/custom/reddrock/js/reddrock.js
@@ -10,10 +10,10 @@
   Drupal.behaviors.reddrock = {
     attach: function(context, settings) {
       var mySound = document.createElement('audio');
-    mySound.setAttribute('src', 'http://dev.reddrockmusic.com/sites/default/files/2018-02/drumtakes%201-Audio.wav');
+      mySound.setAttribute('src', 'http://dev.reddrockmusic.com/sites/default/files/2018-02/drumtakes%201-Audio.wav');
 
-    //play the sound when the visitor moves the mouse over
-    //an element with the class 'picture'. 
+      //play the sound when the visitor moves the mouse over
+      //a menu item. 
     $('.menu--account > li').mouseover(function() {
         mySound.play();
     });
