4582a7f71b7cbd270732609c505a20cb9ca9e24a
cperry-adv <cperry@advanstar.com>
2018-02-05 20:38:25 -0800

    ya

--- modules/custom/reddrock_custom/js/reddrock-custom.js
+++ modules/custom/reddrock_custom/js/reddrock-custom.js
@@ -28,7 +28,7 @@
       //music - the way you see it sounds
       var mainthemeArray = ['018_02_05 HOME - Music_The_Way_You_See_It_01.mp3','018_02_05 HOME - Music_The_Way_You_See_It_02.mp3','018_02_05 HOME - Music_The_Way_You_See_It_03.mp3'];
       var mainthemeSound = document.createElement('audio');
-      mainthemeSound.setAttribute('src', mainthemeArray[Math.floor(Math.random() * mainthemeArray.length)]);
+      mainthemeSound.setAttribute('src', 'http://dev.reddrockmusic.com/sites/default/files/website_sfx/' + mainthemeArray[Math.floor(Math.random() * mainthemeArray.length)]);
       mainthemeSound.setAttribute('id', 'mainthemesound');
       mainthemeSound.volume = 0.2;
       //twitter
