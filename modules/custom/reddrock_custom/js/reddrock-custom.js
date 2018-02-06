fa56465d90342ae9413d5a2082f2f07aa99a7515
cperry-adv <cperry@advanstar.com>
2018-02-05 19:53:26 -0800

    ya

--- modules/custom/reddrock_custom/js/reddrock-custom.js
+++ modules/custom/reddrock_custom/js/reddrock-custom.js
@@ -19,36 +19,13 @@
       hoverSound.setAttribute('src', 'http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Futuristic_Drone.mp3');
       hoverSound.setAttribute('id', 'hoversound');
       hoverSound.volume = 0.2;
-      //leadlick
-      var leadlickSound = document.createElement('audio');
-      leadlickSound.setAttribute('src', 'http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Lead_Lick.mp3');
-      leadlickSound.setAttribute('class', 'logosound');
-      leadlickSound.volume = 0.2;
-      //heavyriff
-      var riffSound = document.createElement('audio');
-      riffSound.setAttribute('src', 'http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Heavy_Riff.mp3');
-      riffSound.setAttribute('class', 'logosound');
-      riffSound.volume = 0.2;
-      //drum1
-      var drum1Sound = document.createElement('audio');
-      drum1Sound.setAttribute('src', 'http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Drum_Take_04.mp3');
-      drum1Sound.setAttribute('class', 'logosound');
-      drum1Sound.volume = 0.2;
-      //drum2
-      var drum2Sound = document.createElement('audio');
-      drum2Sound.setAttribute('src', 'http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Drum_Take_06.mp3');
-      drum2Sound.setAttribute('class', 'logosound');
-      drum2Sound.volume = 0.2;
-      //woo1
-      var woo1Sound = document.createElement('audio');
-      woo1Sound.setAttribute('src', 'http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Diamond_Dave_Woo_01.mp3');
-      woo1Sound.setAttribute('class', 'logosound');
-      woo1Sound.volume = 0.2;
-      //woo2
-      var woo2Sound = document.createElement('audio');
-      woo2Sound.setAttribute('src', 'http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Diamond_Dave_Woo_02.mp3');
-      woo2Sound.setAttribute('class', 'logosound');
-      woo2Sound.volume = 0.2;
+      //logo sounds
+      var logoArray = ['http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Lead_Lick.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Heavy_Riff.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Drum_Take_04.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Drum_Take_06.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Diamond_Dave_Woo_01.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Diamond_Dave_Woo_02.mp3'];
+      var logoSound = document.createElement('audio');
+      logoSound.setAttribute('src', logoArray[Math.floor(Math.random() * logoArray.length)]);
+      logoSound.setAttribute('id', 'logosound');
+      logoSound.volume = 0.2;
+     
 
       var random = Math.floor(Math.random()*10);
       //play hover sound on mouse over main menu items.
@@ -60,7 +37,7 @@
       //  clickSound.play();
       //});
       $('.logo').click(function() {
-        $(".logosound").eq(2).play();
+        logoSound.play();
       });
 
     }
