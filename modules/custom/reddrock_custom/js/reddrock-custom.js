f90c45ab584e5bf7353e70ccf2fd9e8d36a36152
cperry-adv <cperry@advanstar.com>
2018-02-07 15:19:14 -0800

    ya

--- modules/custom/reddrock_custom/js/reddrock-custom.js
+++ modules/custom/reddrock_custom/js/reddrock-custom.js
@@ -19,7 +19,6 @@
       hoverSound.setAttribute('src', 'http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_07 HEADER - Futuristic_Drone ogg.ogg');
       hoverSound.setAttribute('id', 'hoversound');
       hoverSound.loop = true;
-      //hoverSound.loop = true;
       hoverSound.volume = 0.2;
       //logo sounds
       var logoArray = ['http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Lead_Lick.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Heavy_Riff.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Drum_Take_04.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Drum_Take_06.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Diamond_Dave_Woo_01.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Diamond_Dave_Woo_02.mp3'];
@@ -51,14 +50,8 @@
 
       //play hover sound on mouse over main menu items.
       $('.menu--account > li').mouseenter(function() {
-      hoverSound.play();  
-     // hoverSound.addEventListener('timeupdate', function(){
-     //           var buffer = .50
-     //           if(this.currentTime > this.duration - buffer){
-     //               this.currentTime = 0
-      //              this.play()
-      //          }}, false);
-     // });
+        hoverSound.play();
+      });
       $('.menu--account > li').mouseleave(function() {
         hoverSound.pause();
       });
