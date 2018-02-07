2bfafb4498562541352978298215f36fa14c6bb3
cperry-adv <cperry@advanstar.com>
2018-02-07 15:03:49 -0800

    ya

--- modules/custom/reddrock_custom/js/reddrock-custom.js
+++ modules/custom/reddrock_custom/js/reddrock-custom.js
@@ -20,12 +20,6 @@
       hoverSound.setAttribute('id', 'hoversound');
       //hoverSound.loop = true;
       hoverSound.volume = 0.2;
-      hoverSound.addEventListener('timeupdate', function(){
-                var buffer = .84
-                if(this.currentTime > this.duration - buffer){
-                    this.currentTime = 0
-                    this.play()
-                }}, false);
       //logo sounds
       var logoArray = ['http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Lead_Lick.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Heavy_Riff.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Drum_Take_04.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Drum_Take_06.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Diamond_Dave_Woo_01.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Diamond_Dave_Woo_02.mp3'];
       var logoSound = document.createElement('audio');
@@ -56,7 +50,12 @@
 
       //play hover sound on mouse over main menu items.
       $('.menu--account > li').mouseenter(function() {
-        hoverSound.play();
+      hoverSound.addEventListener('timeupdate', function(){
+                var buffer = .44
+                if(this.currentTime > this.duration - buffer){
+                    this.currentTime = 0
+                    this.play()
+                }}, false);
       });
       $('.menu--account > li').mouseleave(function() {
         hoverSound.pause();
