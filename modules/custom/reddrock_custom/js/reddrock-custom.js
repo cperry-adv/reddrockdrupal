d6290918f6a6e5f3c72362033a1ee442fa7bb364
cperry-adv <cperry@advanstar.com>
2018-02-10 13:56:35 -0800

    ya

--- modules/custom/reddrock_custom/js/reddrock-custom.js
+++ modules/custom/reddrock_custom/js/reddrock-custom.js
@@ -119,14 +119,13 @@
       var logoArray = ['http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Lead_Lick.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Heavy_Riff.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Drum_Take_04.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Drum_Take_06.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Diamond_Dave_Woo_01.mp3','http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Diamond_Dave_Woo_02.mp3'];
       var logoSound = document.createElement('audio');
       logoSound.setAttribute('id', 'logosound');
-      logoSound.setAttribute('src', 'http://dev.reddrockmusic.com/sites/default/files/website_sfx/2018_02_05 HEADER - Drum_Take_04.mp3');
       logoSound.volume = 0.2;
       
       $('a.logo > img').once().on({
         mouseover: function() {
             event.preventDefault();
             if (audioOn == 1) {
-          
+          logoSound.setAttribute('src', logoArray[Math.floor(Math.random() * logoArray.length)]);
           logoSound.play();
            console.log(logoSound);
         }
