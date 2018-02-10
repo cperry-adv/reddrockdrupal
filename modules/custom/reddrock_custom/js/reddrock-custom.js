b9061deee17054c6ab8800bd36b322f4c551a06d
cperry-adv <cperry@advanstar.com>
2018-02-09 19:16:01 -0800

    ya

--- modules/custom/reddrock_custom/js/reddrock-custom.js
+++ modules/custom/reddrock_custom/js/reddrock-custom.js
@@ -144,7 +144,7 @@
           history.pushState(null, null, new_url);  
         }
         else {
-          history.pushState(null, null, url[0]);
+          history.pushState(null, null, null);
         }
       }
 
