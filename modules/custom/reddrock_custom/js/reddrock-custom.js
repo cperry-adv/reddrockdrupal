f0e8725f668c46df345c801f0a86c3ac489fb502
cperry-adv <cperry@advanstar.com>
2018-02-09 19:22:51 -0800

    ya

--- modules/custom/reddrock_custom/js/reddrock-custom.js
+++ modules/custom/reddrock_custom/js/reddrock-custom.js
@@ -144,7 +144,7 @@
           history.pushState(null, null, new_url);  
         }
         else {
-          history.replaceState(null, null, boobs);
+          history.replaceState(null, null, 'boobs');
         }
       }
 
