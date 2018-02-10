b75949f39ddefd56c745bec554ae84e143273501
cperry-adv <cperry@advanstar.com>
2018-02-09 23:10:07 -0800

    ya

--- modules/custom/reddrock_custom/js/reddrock-custom.js
+++ modules/custom/reddrock_custom/js/reddrock-custom.js
@@ -85,6 +85,40 @@
       listenarrivalSound.setAttribute('id', 'listenarrivalsound');
       listenarrivalSound.volume = 0.2;
 
+
+$('.filter-tab a').on('click', function(e) {
+        e.preventDefault();
+        
+        // Get ID of clicked item
+        var id = $(e.target).attr('id');
+        
+        // Set the new value in the SELECT element
+        var filter = $('#views-exposed-form-listen-page-block-1 select[name="tid"]');
+        filter.val(id);
+
+        // Unset and then set the active class
+        $('.filter-tab a').removeClass('active');
+        $(e.target).addClass('active');
+
+        // Do it! Trigger the select box
+        //filter.trigger('change');
+        $('#views-exposed-form-listen-page-block-1 select[name="tid"]').trigger('change');
+        $('#views-exposed-form-listen-page-block-1 input.form-submit').trigger('click');
+
+      });
+    }
+  };
+  
+
+  jQuery(document).ajaxComplete(function(event, xhr, settings) {
+  
+        var filter_id = $('#views-exposed-form-listen-page-block-1 select[name="tid"]').find(":selected").val();
+
+        $('.filter-tab a').removeClass('active');
+        $('.filter-tab').find('#' + filter_id).addClass('active');
+
+        
+  });
       
 
       //get current audio switch state from path
